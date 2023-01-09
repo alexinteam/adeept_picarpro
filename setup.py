1f46edcbe6c1a8948fec18099baf367fe782787c
alexey <alem_91@mail.ru>
2023-01-09 23:46:47 +0300

    ver upd

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ for x in range(1,4):
 		break
 
 for x in range(1,4):
-	if os.system("sudo pip3 install opencv-contrib-python==3.4.3.18") == 0:
+	if os.system("sudo pip3 install opencv-contrib-python==3.4.11.45") == 0:
 		break
 
 for x in range(1,4):
